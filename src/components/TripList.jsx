c40d4cbbdee0f4276e88361f4e0f4cebb46fa31e
Giuliano Gianola <giuliano.gianola@swisscom.com>
2024-11-21 12:34:31 +0100

    feat: implement axios and httpService

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {testTrips} from "./api";
-
+import { HttpService } from "../services/httpService";
 
 // functional component ProductList, deconstruct props!
 function TripList({ addToWishlist }) {
+  const [loading, setLoading] = useState(true);
   const [month, setMonth] = useState("");
-  const [trips] = useState(testTrips);
+  const [trips, setTrips] = useState([]);
   const months = ["Idle", "Jan", "Feb", "March", "April", "Mai", "June"];
+
+  useEffect(() => {
+    const httpService = new HttpService();
+    setLoading(true);
+    httpService.getAllTrips().then((data) => {
+      setTrips(data);
+      setLoading(false);
+    });
+  }, []);
 
   const tripsMapped = trips.map((trip, index) => (
     <Trip addToWishlist={addToWishlist} trip={trip} key={trip.id} />
@@ -21,41 +30,47 @@ function TripList({ addToWishlist }) {
 
   // if month selected then filter the trips from month === month
   const filteredTrips = month
-      ? trips.filter((t) => t.startTrip[1] === parseInt(month))
-      : tripsMapped;
+    ? trips.filter((t) => t.startTrip[1] === parseInt(month))
+    : tripsMapped;
 
   return (
     <div className="container">
       <section>
         <h2 className="h4">Triplist-Catalog</h2>
-         <section id="filters">
-            <label htmlFor="month">Filter by Month:</label>
-            <select
-              id="month"
-              value={month} // controlled component
-              onChange={(e) => {
-                //debugger;
-                setMonth(e.target.value);
-              }}
-            >
-              <option value="">All Months</option>
-              <option value="1">January</option>
-              <option value="2">February</option>
-              <option value="3">March</option>
-              <option value="4">April</option>
-              <option value="5">Mai</option>
-              <option value="6">June</option>
-            </select>
-            {month && (
-              <h2>
-                Found {filteredTrips.length}
-                {filteredTrips.length >= 1 ? " trips" : " trip"} for the month of
-                {" " + months[month]}
-              </h2>
-            )}
-          </section>
+        <section id="filters">
+          <label htmlFor="month">Filter by Month:</label>
+          <select
+            id="month"
+            value={month} // controlled component
+            onChange={(e) => {
+              //debugger;
+              setMonth(e.target.value);
+            }}
+          >
+            <option value="">All Months</option>
+            <option value="1">January</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">Mai</option>
+            <option value="6">June</option>
+          </select>
+          {month && (
+            <h2>
+              Found {filteredTrips.length}
+              {filteredTrips.length >= 1 ? " trips" : " trip"} for the month of
+              {" " + months[month]}
+            </h2>
+          )}
+        </section>
         <div className="row">
-          {filteredTrips.length > 0 ? tripsMapped : empty}
+          {loading ? (
+            <p className="alert alert-info">Loading...</p>
+          ) : filteredTrips.length > 0 ? (
+            tripsMapped
+          ) : (
+            empty
+          )}
         </div>
       </section>
     </div>
@@ -64,19 +79,18 @@ function TripList({ addToWishlist }) {
 // deconstruct ...props
 function Trip({ addToWishlist, ...props }) {
   // Props
-  let {trip} = props;
-  let { id, title, description, } = trip;
+  let { trip } = props;
+  let { id, title, description } = trip;
 
   return (
     <div className="col-sm-6 col-md-4 col-lg-3">
       <figure className="card card-product">
-
         <div className="img-wrap">
           <img src={"images/items/" + trip.id + ".jpg"} alt="name " />
         </div>
         <figcaption className="info-wrap">
           <h6 className="title">
-              {id}  {title} {trip.startTrip} {trip.endTrip}
+            {id} {title} {trip.startTrip} {trip.endTrip}
           </h6>
 
           <p className="card-text">{description}</p>
